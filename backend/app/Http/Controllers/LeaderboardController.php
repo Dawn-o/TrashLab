97a6c95d24b660121e7ab89c6d83b5d6582010b8
Rushel <ruselaq@gmail.com>
2025-03-24 18:27:16 +0800

    Enhance LeaderboardController to include current user rank in leaderboard response

--- backend/app/Http/Controllers/LeaderboardController.php
+++ backend/app/Http/Controllers/LeaderboardController.php
@@ -7,22 +7,32 @@ use Illuminate\Http\Request;
 
 class LeaderboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $leaderboard = User::select('id', 'name', 'points')
             ->orderBy('points', 'desc')
             ->limit(10)
-            ->get()
-            ->map(function ($user, $index) {
+            ->get();
+
+        $response = [
+            'leaderboard' => $leaderboard->map(function ($user, $index) {
                 return [
                     'rank' => $index + 1,
                     'name' => $user->name,
                     'points' => $user->points
                 ];
-            });
+            })
+        ];
 
-        return response()->json([
-            'leaderboard' => $leaderboard
-        ]);
+        if ($user = $request->user()) {
+            $userRank = User::where('points', '>', $user->points)->count() + 1;
+            $response['current_user'] = [
+                'rank' => $userRank,
+                'name' => $user->name,
+                'points' => $user->points
+            ];
+        }
+
+        return response()->json($response);
     }
 }
